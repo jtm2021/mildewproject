8c3b16e53bb7879357ace68aa5f71066b562064a
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-09-23 21:53:03 +0000

    Edit content of mildew detector page in dashboard

--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -23,8 +23,8 @@ def page_mildew_detector_body():
 
     st.write("---")
 
-    images_buffer = st.file_uploader('Upload blood smear samples. You may select more than one.',
-                                        type='png',accept_multiple_files=True)
+    images_buffer = st.file_uploader('Upload cherry leaf image. You may select more than one.',
+                                        type='png, jpeg',accept_multiple_files=True)
    
     if images_buffer is not None:
         df_report = pd.DataFrame([])
